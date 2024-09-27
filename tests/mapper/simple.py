3f275d474fe8160742244a1f324cdaddcc9a2a84
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2024-09-27 17:00:40 +0900

    update tests

--- tests/mapper/simple.py
+++ tests/mapper/simple.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-import py2dmat
-import py2dmat.algorithm.mapper_mpi as mapper
-import function
+import odatse
+import odatse.algorithm.mapper_mpi as mapper
+import odatse.extra.template as function
 
-info = py2dmat.Info.from_file("input.toml")
+info = odatse.Info.from_file("input.toml")
 solver = function.Himmelblau(info)
-runner = py2dmat.Runner(solver, info)
+runner = odatse.Runner(solver, info)
 
 alg = mapper.Algorithm(info, runner)
 alg.main()
