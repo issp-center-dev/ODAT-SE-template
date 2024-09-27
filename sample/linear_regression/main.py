77d3d0bdb8d055f886d157bbc4f470411527ecac
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2024-09-27 17:00:51 +0900

    update samples

--- sample/linear_regression/main.py
+++ sample/linear_regression/main.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-import py2dmat
-#import py2dmat.algorithm.mapper_mpi as mapper
-import py2dmat.algorithm.min_search as min_search
-from py2dmat.extra.function import LinearRegression
-import py2dmat.domain
+import odatse
+#import odatse.algorithm.mapper_mpi as mapper
+import odatse.algorithm.min_search as min_search
+from odatse.extra.template import LinearRegression
+import odatse.domain
 
 data = np.loadtxt("data.txt")
 
-info = py2dmat.Info.from_file("input.toml")
+info = odatse.Info.from_file("input.toml")
 solver = LinearRegression(info, xdata=data[:,0], ydata=data[:,1])
-runner = py2dmat.Runner(solver, info)
+runner = odatse.Runner(solver, info)
 
-region = py2dmat.domain.Region(param={"min_list": [-1,-1,-1], "max_list": [1,1,1]})
+region = odatse.domain.Region(param={"min_list": [-1,-1,-1], "max_list": [1,1,1]})
 
 alg = min_search.Algorithm(info, runner, region)
 alg.main()
